822a349b88aa30a696e08bca1144114d6b6600fb
mpalko_astound <m.palko@astoundcommerce.com>
2024-08-20 14:16:12 +0200

    Buttons action

--- src/components/actionButtons.tsx
+++ src/components/actionButtons.tsx
@@ -1,11 +1,46 @@
 import Button from "./button.tsx";
+import { useSelector, useDispatch } from "react-redux";
+import { initTask, pauseTask, stopTask} from "../feature/tasksSlice.ts";
+import { RootState } from "../store.ts"
 
 const ActionButtons = () => {
+    const workStatus = useSelector((state: RootState) => state.tasks.workStatus)
+    const dispatch = useDispatch();
+
+    const startHandler = () => {
+        dispatch(initTask())
+    }
+    const pauseHandler = () => {
+        dispatch(pauseTask())
+    }
+    const stopHandler = () => {
+        dispatch(stopTask())
+    }
+
     return (
         <div className="actions">
-            <Button className="btn actions-btn actions-start" />
-            <Button className="btn actions-btn actions-pause" />
-            <Button className="btn actions-btn actions-stop" />
+
+            {(workStatus === "pause" || workStatus === "stop" || workStatus === "standby") && (
+                <Button
+                    className="btn actions-btn actions-start"
+                    onClick={() => startHandler()}
+                />
+            )}
+
+            {workStatus === "work" && (
+                <Button
+                    className="btn actions-btn actions-pause"
+                    onClick={() => pauseHandler()}
+                />
+            )}
+
+            {(workStatus === "pause" || workStatus === "work" || workStatus === "initiation") && (
+                <Button
+                    className="btn actions-btn actions-stop"
+                    onClick={() => stopHandler()}
+                />
+            )}
+
         </div>
     )
 }
